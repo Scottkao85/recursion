714c493b3fe34ab9f313a9b61cdf63811aaffeec
Scott Kao <scottkao85@gmail.com>
2014-12-31 18:08:55 -0800

    finished primitive values and array

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,8 +1,34 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
 
+
+// but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  var result;
+  if(typeof obj === "number"){
+    result = obj.toString();
+  }else if(obj === null){
+    result = "null";
+  }else if(obj === true){//"boolean"
+    result = "true" //" \" " + obj + " \" "; 
+  }else if(obj === false){//"boolean"
+    result = "false"; 
+  }else if(typeof obj === "string"){
+    result =  "\"" + obj + "\"" ;
+  }else if(Array.isArray(obj) === true){
+    if(obj.length === 0){
+      result = "[]"//"\"" + "[]" + "\"";
+    }else {
+      for(var i = 0; i < obj.length; i++){
+        if(i === 0){
+          result = "[" + stringifyJSON(obj[i])
+        }else{ 
+          result = result + "," + stringifyJSON(obj[i])
+        }
+      } 
+      result = result + "]"
+    }
+  }
+  return result;
+}
